61ae6f5c456da54d7de3a987b37add59aa5d6679
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-07-14 17:05:36 -0700

    Rename 'model' variable to 'resource', which is what it is.

--- lib/dm-accepts_nested_attributes/relationship.rb
+++ lib/dm-accepts_nested_attributes/relationship.rb
@@ -4,8 +4,8 @@ module DataMapper
     # Extensions for {DataMapper::Associations::Relationship}.
     module Relationship
       # Extracts the primary key values necessary to retrieve or update a nested
-      # model when using {Model#accepts_nested_attributes_for}. Values are taken from
-      # the specified model and attribute hash with the former having priority.
+      # resource when using {Model#accepts_nested_attributes_for}. Values are taken from
+      # the specified resource and attribute hash with the former having priority.
       # Values for properties in the primary key that are *not* included in the
       # foreign key must be specified in the attributes hash.
       #
@@ -14,20 +14,20 @@ module DataMapper
       #
       # @param [Hash] attributes
       #   The attributes assigned to the nested attribute setter on the
-      #   +model+.
+      #   +resource+.
       #
       # @return [Array, NilClass]
       #   Array if valid key values are present, nil otherwise
       # 
       # @api private
-      def extract_keys_for_nested_attributes(model, attributes)
+      def extract_keys_for_nested_attributes(resource, attributes)
         target_model_key = self.target_model.key
         target_key_array = self.target_key.to_a
         source_key_array = self.source_key.to_a
 
         key_values = target_model_key.to_enum(:each_with_index).map do |key, idx|
           if source_idx = target_key_array.index(key)
-            model[source_key_array.at(source_idx).name]
+            resource[source_key_array.at(source_idx).name]
           else
             attributes[key.name]
           end
@@ -65,7 +65,7 @@ module DataMapper
     # Extensions for {DataMapper::Associations::ManyToMany::Relationship}.
     module ManyToMany
       # @api private
-      def extract_keys_for_nested_attributes(model, attributes)
+      def extract_keys_for_nested_attributes(resource, attributes)
         key_values = self.child_key.map do |key|
           attributes[key.name]
         end
